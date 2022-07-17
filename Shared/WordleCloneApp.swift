96b75043615206c6a8fd0682f2acedfdeeb576d6
Rob Ranf <rob@emiyaconsulting.com>
2022-07-17 12:11:30 -0700

    Initial Commit

---
+++ Shared/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  Shared
+//
+//  Created by Rob Ranf on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
