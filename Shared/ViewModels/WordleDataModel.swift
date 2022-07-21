f48c047c040f29b0b19adea196ebbacc8b849339
Rob Ranf <rob@emiyaconsulting.com>
2022-07-21 14:50:53 -0700

    Added list of words and validation against Apple dictionary

--- Shared/ViewModels/WordleDataModel.swift
+++ Shared/ViewModels/WordleDataModel.swift
@@ -11,13 +11,26 @@ class WordleDataModel: ObservableObject {
     /**
      @Published is one of the most useful property wrappers in SwiftUI, allowing us to create observable objects that automatically announce when changes occur. SwiftUI will automatically monitor for such changes, and re-invoke the body property of any views that rely on the data.
      
-     In practical terms, that means whenever an object with a property marked @Published is changed, all views using that object will be reloaded to reflect those changes. **You can think of properties marked with @Published property wrapper as similar
-     to the $emit event in Vue as they accomplish the same result in that they tell a "component" (a view in Swift) that some other "component" (in this case a ViewModel) has had a change in state that requires that other "component" to receive that change and in the case of Swift, reload the view.**
+     In practical terms, that means whenever an object with a property marked @Published is changed, all views using that object will be reloaded to reflect those changes. **You can think of properties marked with @Published property wrapper as similar to the $emit event in Vue as they accomplish the same result in that they tell a "component" (a view in Swift) that some other "component" (in this case a ViewModel) has had a change in state that requires that other "component" to receive that change and in the case of Swift, reload the view.**
      (https://www.hackingwithswift.com/quick-start/swiftui/what-is-the-published-property-wrapper)
      */
     @Published var guesses: [Guess] = []
+    @Published var incorrectAttempts = [Int](repeating: 0, count: 6)
     
     var keyColors = [String: Color]()
+    var selectedWord = ""
+    var currentWord = ""
+    var tryIndex = 0
+    var inPlay = false
+    
+    var gameStarted: Bool {
+        !currentWord.isEmpty || tryIndex > 0
+    }
+    
+    var disabledKeys: Bool {
+        !inPlay || currentWord.count == 5
+    }
+    
     
     init() {
         newGame()
@@ -26,6 +39,9 @@ class WordleDataModel: ObservableObject {
     //MARK: - Setup
     func newGame() {
         populateDefaults()
+        selectedWord = Global.availableWords.randomElement()!
+        currentWord = ""
+        inPlay = true
     }
     
     func populateDefaults() {
@@ -42,14 +58,33 @@ class WordleDataModel: ObservableObject {
     
     //MARK: - Game Play
     func addToCurrentWord(_ letter: String) {
-        
+        currentWord += letter
+        updateRow()
     }
     
     func enterWord() {
-        
+        if verifyWord() {
+            let _ = print("Valid word")
+        } else {
+            withAnimation {
+                self.incorrectAttempts[tryIndex] += 1
+            }
+            incorrectAttempts[tryIndex] = 0
+            let _ = print("Invalid word")
+        }
     }
     
     func removeLastLetterFromCurrentWord() {
-        
+        currentWord.removeLast()
+        updateRow()
+    }
+    
+    func updateRow() {
+        let guessWord = currentWord.padding(toLength: 5, withPad: " ", startingAt: 0)
+        guesses[tryIndex].word = guessWord
+    }
+    
+    func verifyWord() -> Bool {
+        UIReferenceLibraryViewController.dictionaryHasDefinition(forTerm: currentWord)
     }
 }
--- Shared/Views/Keyboard.swift
+++ Shared/Views/Keyboard.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct Keyboard: View {
     @EnvironmentObject var dm: WordleDataModel
-    var topRowArray = "QWERTYUIOP".map{ String($0) }
+    // Using shorthand argument names; map takes a closure as its argument (because it's only one argument, we can remove the () that enclose arguments). In this case, our shorthand argument names in middleRowArray and bottomRowArray are the same as how topRowArray is written.
+    var topRowArray = "QWERTYUIOP".map({char in
+        String(char) //Same thing as saying "QWERTYUIOP"[char] or, in a correct syntax, first assigning "QWERTYUIOP" to varName then varName[char]
+    })
     var middleRowArray = "ASDFGHJKL".map{ String($0) }
     var bottomRowArray = "ZXCVBNM".map{ String($0) }
         var body: some View {
@@ -18,11 +21,15 @@ struct Keyboard: View {
                     ForEach(topRowArray, id: \.self) { letter in
                         LetterButtonView(letter: letter)
                     }
+                    .disabled(dm.disabledKeys)
+                    .opacity(dm.disabledKeys ? 0.6 : 1)
                 }
                 HStack(spacing: 2) {
                     ForEach(middleRowArray, id: \.self) { letter in
                         LetterButtonView(letter: letter)
                     }
+                    .disabled(dm.disabledKeys)
+                    .opacity(dm.disabledKeys ? 0.6 : 1)
                 }
                 HStack(spacing: 2) {
                     Button {
@@ -34,18 +41,24 @@ struct Keyboard: View {
                     .frame(width: 60, height: 50)
                     .foregroundColor(.primary)
                     .background(Color.unused)
+                    .disabled(dm.currentWord.count < 5 || !dm.inPlay)
+                    .opacity((dm.currentWord.count < 5 || !dm.inPlay) ? 0.6 : 1)
                     ForEach(bottomRowArray, id: \.self) { letter in
                         LetterButtonView(letter: letter)
                     }
+                    .disabled(dm.disabledKeys)
+                    .opacity(dm.disabledKeys ? 0.6 : 1)
                     Button {
                         dm.removeLastLetterFromCurrentWord()
                     } label: {
                         Image(systemName: "delete.backward.fill")
+                            .font(.system(size: 20, weight: .heavy))
+                            .frame(width: 40, height: 50)
+                            .foregroundColor(.primary)
+                            .background(Color.unused)
                     }
-                    .font(.system(size: 20, weight: .heavy))
-                    .frame(width: 40, height: 50)
-                    .foregroundColor(.primary)
-                    .background(Color.unused)
+                    .disabled(!dm.inPlay || dm.currentWord.count == 0)
+                    .opacity((!dm.inPlay || dm.currentWord.count == 0) ? 0.6 : 1)
                 }
             }
     }
